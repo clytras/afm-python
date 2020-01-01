8881e8d9c93cb1350e09de8ca2316cab3d8099ca
Christos Lytras <christos.lytras@gmail.com>
2020-01-01 22:47:18 +0200

    proper setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = 'lytrax_afm',
     version = '1.0.0',
-    packages = find_packages()
+    author="Christos Lytras",
+    author_email="christos.lytras@gmail.com",
+    description="Greek AFM (TIN) Validator and Generator",
+    url="https://github.com/clytras/afm-python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages = find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
 )
